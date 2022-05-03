460a458ee1bfd96656e096063ca732d2ae86615c
FlutLab.io <>
2022-05-03 14:30:34 +0000

    Autosave

---
+++ lib/galeriadebusses.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class PaginaPrincipal extends StatefulWidget {
+  PaginaPrincipal({Key? key}) : super(key: key);
+  @override
+  _PaginaPrincipalState createState() => _PaginaPrincipalState();
+} //widget con estado
+
+class _PaginaPrincipalState extends State<PaginaPrincipal> {
+  List<String> images = [
+    "assets/1.jpg",
+    "assets/2.jpg",
+    "assets/3.jpg",
+    "assets/4.jpg",
+    "assets/5.jpg",
+    "assets/6.jpg",
+    "assets/7.jpg",
+    "assets/8.jpg",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Tutorial GridView"),
+        ),
+        body: GridView.custom(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          childrenDelegate: SliverChildBuilderDelegate(
+            (BuildContext, index) {
+              return Image.asset(
+                images[index],
+                fit: BoxFit.cover,
+              );
+            },
+            childCount: 8,
+          ),
+          padding: EdgeInsets.all(10),
+          shrinkWrap: true,
+        ));
+  }
+}
